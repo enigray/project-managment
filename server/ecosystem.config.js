caacb4162e5ee586d3b6aa56cd60e127a86885db
Eni Gray <enigray05@gmail.com>
2024-12-13 23:05:42 +0100

    somthing added

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  apps: [
+    {
+      name: "project-management",
+      script: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
+
+module.exports = {
     apps: [
         {
             name:"project-management",
